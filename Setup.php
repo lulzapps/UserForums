c563609d7d3d723c3080d3168b53b5f9bac00e57
Adalid Claure <aclaure@gmail.com>
2022-12-30 11:34:45 -0500

    updating

--- Setup.php
+++ Setup.php
@@ -7,9 +7,33 @@ use XF\AddOn\StepRunnerInstallTrait;
 use XF\AddOn\StepRunnerUninstallTrait;
 use XF\AddOn\StepRunnerUpgradeTrait;
 
+use XF\Db\Schema\Alter;
+use XF\Db\Schema\Create;
+
 class Setup extends AbstractSetup
 {
 	use StepRunnerInstallTrait;
 	use StepRunnerUpgradeTrait;
 	use StepRunnerUninstallTrait;
+	
+    public function installStep1()
+    {
+        $this->schemaManager()->alterTable('xf_forum', function(Alter $table)
+        {
+            $table->addColumn('lz_userforums_owner_id', 'int')->setDefault(0);
+        });
+	}
+
+    public function installStep2()
+    {
+        $this->schemaManager()->createTable('lz_userforums_moderators', 
+            function(Create $table)
+            {
+                $table->addColumn('moderator_id', 'int')->autoIncrement();
+                $table->addColumn('node_id', 'int');
+                $table->addColumn('user_id', 'int');
+                $table->addColumn('role', 'tinyint')->setDefault(0);
+                $table->addPrimaryKey('moderator_id');
+            });
+    }    
 }
