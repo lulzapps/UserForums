04342a50d180747f34f84dca211d6d51cbdd12e7
Adalid Claure <adalid.claure@rsa.com>
2022-12-30 21:25:11 -0500

    more work

--- Pub/Controller/Manage.php
+++ Pub/Controller/Manage.php
@@ -4,6 +4,14 @@ namespace lulzapps\UserForums\Pub\Controller;
 
 class Manage extends \XF\Pub\Controller\AbstractController
 {
+    const FORUM_NAME_MAX_LENGTH = 25;
+    const FORUM_NAME_REGEX = '#^[a-zA-z0-9_]{3,' . self::FORUM_NAME_MAX_LENGTH . '}$#';
+
+    const FORUM_TITLE_MAX_LENGTH = 50;
+    const FORUM_TITLE_REGEX = '#^.{3,' . self::FORUM_TITLE_MAX_LENGTH . '}$#';
+
+    const FORUM_DESC_MAX_LENGTH = 255;
+
     public function actionCreate()
     {
         $input = $this->filter([
@@ -13,19 +21,38 @@ class Manage extends \XF\Pub\Controller\AbstractController
             'forum_type' => 'str',
         ]);
 
-        $msg = "$input[forum_name] $input[forum_title] $input[forum_desc] $input[forum_type]";
+        if (preg_match(self::FORUM_NAME_REGEX, $input['forum_name']) != 1)
+        {
+            return $this->error('TODO: create phrase for invalid forum name error');
+        }
 
-        throw $this->exception($this->error($msg));
-		// $viewParams = [
-		// ];
-		// return $this->view('XF:Warning\Info', 'warning_info', $viewParams);
+        if (preg_match(self::FORUM_TITLE_REGEX, $input['forum_title']) != 1)
+        {
+            return $this->error('TODO: create phrase for invalid forum title error');
+        }
+
+        if (strlen($input['forum_desc']) > self::FORUM_DESC_MAX_LENGTH)
+        {
+            return $this->error('TODO: create phrase for invalid forum description error');
+        }
+
+        if ($input['forum_type'] != 'public' && $input['forum_type'] != 'private' && $input['forum_type'] != 'restricted')
+        {
+            return $this->error('TODO: create phrase for invalid forum type error');
+        }
+
+        $msg = "$input[forum_name] $input[forum_title] $input[forum_desc] $input[forum_type]";
+        return $this->error($msg);
     }
 
     public function actionIndex()
     {
         $viewParams = 
-        [
-            'createUrl' => $this->buildLink('manage/create')
+        [   
+            'name_max_length' => self::FORUM_NAME_MAX_LENGTH,
+            'title_max_length' => self::FORUM_TITLE_MAX_LENGTH,
+            'desc_max_length' => self::FORUM_DESC_MAX_LENGTH,
+            'createUrl' => $this->buildLink('userforums/create')
         ];
 
         return $this->view('lulzapps\UserForums:View', 'lz_userforums_manage_view', $viewParams);
